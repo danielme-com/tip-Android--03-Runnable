b1bde5bac28b52b98a6061c0552dce72c44cf68b
danielme-com <daniel_com@yahoo.com>
2015-07-28 20:27:47 +0200

    control de salida de la Activity

--- src/com/danielme/tipsandroid/runnable/MainActivity.java
+++ src/com/danielme/tipsandroid/runnable/MainActivity.java
@@ -18,119 +18,85 @@ import android.widget.ProgressBar;
 
 import com.danieme.tipsandroid.runnable.R;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
 	ProgressBar progressBar;
 	ImageView imageView;
+	//permite saber dentro del hilo y handler si se ha salido de la Activity
+	volatile boolean endThread = false;
 	MyHandler myHandler = new MyHandler(MainActivity.this);
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		progressBar = (ProgressBar) findViewById(R.id.progresss);
 		imageView = (ImageView) findViewById(R.id.imageView1);
 		progressBar.setVisibility(View.VISIBLE);
 
-		new Thread()
-		{
-			public void run()
-			{
+		new Thread() {
+			public void run() {
 				Message message = new Message();
-				try
-				{
-					//espera de 3 segundos para que de tiempo a ver el progress
+				try {
+					// espera de 3 segundos para que de tiempo a ver el progress
 					Thread.sleep(3000);
-			    	URL url = new URL("http://danielmedotcom.files.wordpress.com/2012/02/icon-danielme.png");
-			    	Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-			    	
-					message.obj = bmp;
-					message.what = 1;				
-				}
-				catch (Exception ex)
-				{
+					if (!endThread) {
+						URL url = new URL(
+								"https://danielmedotcom.files.wordpress.com/2013/01/android-tip.png");
+						Bitmap bmp = BitmapFactory.decodeStream(url
+								.openConnection().getInputStream());
+
+						message.obj = bmp;
+						message.what = 1;
+					}
+				} catch (Exception ex) {
 					Log.e(MainActivity.class.toString(), ex.getMessage(), ex);
 					message.what = 2;
-					
-				}
-				finally
-				{					
-					myHandler.sendMessage(message);
+				} finally {
+					if (!endThread) {
+						myHandler.sendMessage(message);
+					}
 				}
 			}
 
 		}.start();
 	}
-		
-	//Solución estática
-	private static class MyHandler extends Handler 
-	{
-	    private final WeakReference<MainActivity> myActivity;
-	    
-	    public MyHandler(MainActivity myActivity) 
-	    {
-	    	this.myActivity = new WeakReference<MainActivity>(myActivity);
-	    }	
-	    
-	    @Override
-		public void handleMessage(Message msg)
-		{
-			 //la tarea en segundo plano ya ha terminado. Ocultamos el progreso.
-	    	myActivity.get().progressBar.setVisibility(View.GONE);
-			 //si tenemos la imagen la mostramos
-			 if (msg.what == 1 && msg.obj != null)
-			 {
-				 myActivity.get().imageView.setImageBitmap((Bitmap) msg.obj);
-				 myActivity.get().imageView.setVisibility(View.VISIBLE);
-			 }
-			 //si no, informamos del error
-			 else
-			 {
-				 	Builder builder = new Builder(myActivity.get());
-					builder.setTitle(R.string.title);
-					builder.setIcon(android.R.drawable.ic_dialog_info);
-					builder.setMessage(R.string.error);					
-					builder.setNeutralButton(myActivity.get().getString(R.string.ok), null);			
 
-					AlertDialog alertDialog = builder.create();
-					alertDialog.show();
-					alertDialog.setCancelable(false);
-			 }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		endThread = true;
+	}
+
+	private static class MyHandler extends Handler {
+		private final WeakReference<MainActivity> myActivity;
+
+		public MyHandler(MainActivity myActivity) {
+			this.myActivity = new WeakReference<MainActivity>(myActivity);
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			// la tarea en segundo plano ya ha terminado. Ocultamos el progreso.
+			myActivity.get().progressBar.setVisibility(View.GONE);
+			// si tenemos la imagen la mostramos
+			if (msg.what == 1 && msg.obj != null) {
+				myActivity.get().imageView.setImageBitmap((Bitmap) msg.obj);
+				myActivity.get().imageView.setVisibility(View.VISIBLE);
+			}
+			// si no, informamos del error
+			else {
+				Builder builder = new Builder(myActivity.get());
+				builder.setTitle(R.string.title);
+				builder.setIcon(android.R.drawable.ic_dialog_info);
+				builder.setMessage(R.string.error);
+				builder.setNeutralButton(myActivity.get()
+						.getString(R.string.ok), null);
+
+				AlertDialog alertDialog = builder.create();
+				alertDialog.show();
+				alertDialog.setCancelable(false);
+			}
 		}
 	}
-	
-	//no estático
-	/*private Handler getHandler = new Handler()
-	{
-		@Override
-		public void handleMessage(Message msg)
-		{
-			 //la tarea en segundo plano ya ha terminado. Ocultamos el progreso.
-			 progressBar.setVisibility(View.GONE);
-			 //si tenemos la imagen la mostramos
-			 if (msg.what == 1 && msg.obj != null)
-			 {
-				imageView.setImageBitmap((Bitmap) msg.obj);
-				imageView.setVisibility(View.VISIBLE);
-			 }
-			 //si no, informamos del error
-			 else
-			 {
-				 	Builder builder = new Builder(MainActivity.this);
-					builder.setTitle(R.string.title);
-					builder.setIcon(android.R.drawable.ic_dialog_info);
-					builder.setMessage(R.string.error);					
-					builder.setNeutralButton(getString(R.string.ok), null);			
 
-					AlertDialog alertDialog = builder.create();
-					alertDialog.show();
-					alertDialog.setCancelable(false);
-			 }
-			
-
-		}
-
-	};*/
-	
 }
